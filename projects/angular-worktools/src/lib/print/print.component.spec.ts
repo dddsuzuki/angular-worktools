dada07c19465f7fdfb715f8c17278c9369d7fa8a
Daisuke Suzuki <35087637+dddsuzuki@users.noreply.github.com>
2020-12-28 14:38:28 +0900

    chore: update to angular 11.0.5

--- projects/angular-worktools/src/lib/print/print.component.spec.ts
+++ projects/angular-worktools/src/lib/print/print.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WtPrintComponent } from './print.component';
 
@@ -6,7 +6,7 @@ describe('WtPrintComponent', () => {
   let component: WtPrintComponent;
   let fixture: ComponentFixture<WtPrintComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WtPrintComponent ]
     })
